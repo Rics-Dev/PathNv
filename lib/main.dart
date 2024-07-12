16c9801979b68b9061163036cc815461e569d615
racimfethallah <ricdev.io@gmail.com>
2024-07-12 18:09:20 +0100

    small updates

--- lib/main.dart
+++ lib/main.dart
@@ -36,20 +36,38 @@ class _Home extends StatefulWidget {
 
 class _HomeState extends State<_Home> {
   List<Paths> paths = [];
+  List<Paths> filteredPaths = [];
   bool isRefreshing = false;
   bool isSearching = false;
+  String searchQuery = '';
+  String shell = Platform.environment['SHELL'] ?? 'sh';
 
   void runShellCommand() async {
-    String shell = Platform.environment['SHELL'] ?? 'sh';
     String command = 'echo \$PATH';
 
-    ProcessResult result = await Process.run(shell, ['-i', '-c', command]);
+    ProcessResult result =
+        await Process.run(shell, ['-l', '-i', '-c', command]);
 
     String pathResult = result.stdout.trim();
 
     setState(() {
       paths = pathResult.split(':').map((path) => Paths(path)).toList();
+      filteredPaths = List.from(paths);
       isRefreshing = false;
+    });
+  }
+
+  void filterPaths(String query) {
+    setState(() {
+      searchQuery = query;
+      if (query.isEmpty) {
+        filteredPaths = List.from(paths);
+      } else {
+        filteredPaths = paths
+            .where(
+                (path) => path.path.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      }
     });
   }
 
@@ -67,7 +85,53 @@ class _HomeState extends State<_Home> {
           child: YaruOptionButton(
             child: const Icon(LucideIcons.plus),
             onPressed: () {
-              print("Pressed");
+              showDialog(
+                context: context,
+                builder: (context) => SimpleDialog(
+                  contentPadding:
+                      const EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 16.0),
+                  title: const Text("Add Path"),
+                  children: [
+                    YaruTile(
+                      leading: YaruIconButton(
+                        icon: const Icon(LucideIcons.folder),
+                        onPressed: () async {
+                          String? selectedDirectory =
+                              await FilePicker.platform.getDirectoryPath();
+                          print(selectedDirectory);
+                        },
+                      ),
+                      title: const SizedBox(
+                        width: 500,
+                        child: YaruSearchField(
+                          style: YaruSearchFieldStyle.filledOutlined,
+                          radius: Radius.circular(8),
+                          hintText: "Enter path",
+                          text: "",
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Cancel"),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Add"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
             },
           ),
         ),
@@ -79,6 +143,8 @@ class _HomeState extends State<_Home> {
             onPressed: () {
               setState(() {
                 isSearching = !isSearching;
+                searchQuery = '';
+                filteredPaths = List.from(paths);
               });
             },
           ),
@@ -124,8 +190,9 @@ class _HomeState extends State<_Home> {
           )
         ],
         title: isSearching
-            ? const YaruSearchField(
-                text: "",
+            ? YaruSearchField(
+                text: searchQuery,
+                onChanged: filterPaths,
               )
             : const Text("PathNv"),
       ),
@@ -135,22 +202,22 @@ class _HomeState extends State<_Home> {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Row(
+              Row(
                 children: [
-                  Icon(LucideIcons.terminal),
-                  SizedBox(
+                  const Icon(LucideIcons.terminal),
+                  const SizedBox(
                     width: 8,
                   ),
-                  Text(
-                    "Paths",
-                    style: TextStyle(
+                  SelectableText(
+                    "${shell.split('/').last} ${filteredPaths.length} paths",
+                    style: const TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
               ),
-              for (Paths path in paths)
+              for (Paths path in filteredPaths)
                 YaruTile(
                   style: YaruTileStyle.normal,
                   leading: path.isEditing
