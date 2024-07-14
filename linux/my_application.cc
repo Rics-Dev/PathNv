603d5cc6f7094530ae92d2b51ce212dfb0991fe8
racimfethallah <ricdev.io@gmail.com>
2024-07-14 18:24:03 +0100

    small changes

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -87,8 +87,7 @@ static void my_application_class_init(MyApplicationClass* klass) {
 static void my_application_init(MyApplication* self) {}
 
 MyApplication* my_application_new() {
-  return MY_APPLICATION(g_object_new(
-      my_application_get_type(), "application-id", APPLICATION_ID, "flags",
-      G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN,
-      nullptr));
+  return MY_APPLICATION(g_object_new(my_application_get_type(),
+                                     "application-id", APPLICATION_ID,
+                                     nullptr));
 }
